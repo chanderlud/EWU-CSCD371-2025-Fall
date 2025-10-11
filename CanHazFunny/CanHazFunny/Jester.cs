62a034bae4fba444f36b4c0dc6dfc8503bbbad5f
Sean H <DjPigPigstar@gmail.com>
2025-10-11 10:20:36 -0700

    Added Required Interfaces

---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny;
+public class Jester
+{
+    private IJokeOutput JokeOutput;
+    private IJokeService JokeService;
+
+    public Jester(IJokeOutput jokeOutput, IJokeService jokeService)
+    {
+        JokeOutput = jokeOutput;
+        JokeService = jokeService;
+    }
+
+    public void TellJoke()
+    {
+        JokeOutput.PrintJoke(JokeService.GetJoke());
+    }
+}
---
+++ CanHazFunny/CanHazFunny/JokeOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny;
+
+
+internal class JokeOutput : IJokeOutput
+{
+    public void PrintJoke(string joke)
+    {
+        Console.WriteLine(joke);
+    }
+}
+
