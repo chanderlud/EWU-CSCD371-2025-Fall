478f97baeff99d3a2871137f01f3053a61aec5af
Ulises <54950806+ulises-aguilar@users.noreply.github.com>
2025-11-02 12:20:00 -0800

    Add initial implementation of Program class

---
+++ Calculate.Tests/ProgramTests.cs
@@ -0,0 +1,1 @@
+namespace Calculate.Tests;
---
+++ Calculate/Program.cs
@@ -0,0 +1,18 @@
+namespace Calculate;
+
+public class Program
+{
+
+    public Action<string> WriteLine { get; init; } = Console.WriteLine;
+    public Func<string?> ReadLine { get; init; } = Console.ReadLine;
+    public Program() { }
+    
+    public static void Main(string[] args)
+    {
+        
+        
+    }
+
+
+
+}
